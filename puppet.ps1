0e1a2df7d89d6192df928281554213d4150335d1
Alexander Tsirel <noma4i@gmail.com>
2015-01-30 11:10:27 +1100

    puppet oneliner test

---
+++ puppet.ps1
@@ -0,0 +1,11 @@
+ param (
+    [string]$p_host = $(throw "-p_host is required!"),
+    [string]$p_ip = $(throw "-p_id is required!")
+ )
+
+`n`t{0}`t{1}" -f $p_ip.replace(" ",""), $p_host.replace(" ","") | out-file C:\Windows\System32\drivers\etc\hosts -append
+
+# $source = "https://downloads.puppetlabs.com/windows/puppet-latest.msi"
+# $destination = "c:\temp\puppet.msi"
+
+# Invoke-WebRequest $source -OutFile $destination
