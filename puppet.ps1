fc9a0e8ec542683a35301b9dc43661afd86ad6db
Alexander Tsirel <noma4i@gmail.com>
2015-01-30 11:53:51 +1100

    interactive input

--- puppet.ps1
+++ puppet.ps1
@@ -1,6 +1,4 @@
- param (
-    [string]$p_host = $(throw "-p_host is required!"),
-    [string]$p_ip = $(throw "-p_id is required!")
- )
-
+# (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/noma4i/powershell-toolbelt/master/puppet.ps1", "c:\temp\puppet.ps1") | powershell c:\temp\puppet.ps1
+$p_host = Read-Host 'Puppet Host'
+$p_ip = Read-Host 'Puppet IP'
 $p_ip + "`t`t" + $p_host | Out-File -encoding ASCII -append "C:\Windows\System32\drivers\etc\hosts"
