6cf65f4574d5ae963f96bd69a30f99a19a4fe88b
Alexander Tsirel <noma4i@gmail.com>
2015-01-30 12:04:07 +1100

    puppet is a default hostname

--- puppet.ps1
+++ puppet.ps1
@@ -1,6 +1,6 @@
 # (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/noma4i/powershell-toolbelt/master/puppet.ps1", "c:\temp\puppet.ps1") | powershell c:\temp\puppet.ps1
-$p_host = Read-Host 'Puppet Host'
-$p_ip = Read-Host 'Puppet IP'
+$p_host = "puppet"
+$p_ip = Read-Host 'Puppetmaster IP'
 $p_ip + "`t`t" + $p_host | Out-File -encoding ASCII -append "C:\Windows\System32\drivers\etc\hosts"
 
 $source = "https://downloads.puppetlabs.com/windows/puppet-latest.msi"
