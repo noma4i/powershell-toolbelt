a929418cfe6f7144dcdce1792cc1433d02538cd5
Alexander Tsirel <noma4i@gmail.com>
2015-01-30 11:13:55 +1100

    --wip--

--- puppet.ps1
+++ puppet.ps1
@@ -4,8 +4,3 @@
  )
 
 `n`t{0}`t{1}" -f $p_ip.replace(" ",""), $p_host.replace(" ","") | out-file C:\Windows\System32\drivers\etc\hosts -append
-
-# $source = "https://downloads.puppetlabs.com/windows/puppet-latest.msi"
-# $destination = "c:\temp\puppet.msi"
-
-# Invoke-WebRequest $source -OutFile $destination
